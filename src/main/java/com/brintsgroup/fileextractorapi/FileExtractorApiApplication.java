40109672467acecee3a1f5476fd38c02284716f4
aniebietafia <aniebietafia87@gmail.com>
2025-02-08 18:17:16 +0100

    initializing the project using spring initializer

---
+++ src/main/java/com/brintsgroup/fileextractorapi/FileExtractorApiApplication.java
@@ -0,0 +1,13 @@
+package com.brintsgroup.fileextractorapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileExtractorApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileExtractorApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/brintsgroup/fileextractorapi/FileExtractorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brintsgroup.fileextractorapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileExtractorApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
